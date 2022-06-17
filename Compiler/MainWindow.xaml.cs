a0861fe2cd5db21163aa63d99d1f90b09da20a78
Ibragf <gaziibragim@mail.ru>
2022-06-17 21:13:05 +0300

    add example

--- Compiler/MainWindow.xaml.cs
+++ Compiler/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ namespace Compiler
         {
             InitializeComponent();
             uploadFormula();
+            uploadExample();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -168,15 +169,30 @@ namespace Compiler
             }
         }
 
-        public async void uploadFormula()
+        public void uploadFormula()
         {
             using (FileStream fstream = File.OpenRead("formula.txt"))
             {
                 byte[] buffer = new byte[fstream.Length];
-                await fstream.ReadAsync(buffer,0, buffer.Length);
+                fstream.Read(buffer,0, buffer.Length);
                 formula.Text=Encoding.UTF8.GetString(buffer);
             }
         }
 
+
+        public void uploadExample()
+        {
+            using(FileStream fs=File.OpenRead("example.txt"))
+            {
+                byte[] buffer=new byte[fs.Length];
+                fs.Read(buffer,0,buffer.Length);
+
+                Paragraph paragraph = new Paragraph();
+                InputText.Document.Blocks.Clear();
+                InputText.Document.Blocks.Add(paragraph);
+
+                paragraph.Inlines.Add(new Run(Encoding.UTF8.GetString(buffer)));
+            }
+        }
     }
 }
